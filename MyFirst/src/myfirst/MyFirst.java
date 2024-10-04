374ec2e935146cb0d1d99a5a4ab8a638913fb382
Bear-ium <98127529+Bear-ium@users.noreply.github.com>
2024-10-04 09:06:01 +0200

    Print Statements
    Were taught about Print and PrintLn statements

--- MyFirst/src/myfirst/MyFirst.java
+++ MyFirst/src/myfirst/MyFirst.java
@@ -15,6 +15,9 @@ public class MyFirst {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Greetings");
+        System.out.print("Winston is fire 🔥🔥🔥🔥");
+        System.out.println(" >^~^< ");
     }
     
 }
