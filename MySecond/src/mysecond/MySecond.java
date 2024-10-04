b33aba5c88f6d5e31c341c173f9b9d4d1d66595d
Bear-ium <98127529+Bear-ium@users.noreply.github.com>
2024-10-04 10:28:29 +0200

    worksheet 2

--- MySecond/src/mysecond/MySecond.java
+++ MySecond/src/mysecond/MySecond.java
@@ -2,22 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package myfirst;
+package mysecond;
 
 /**
  *
  * @author Kyle
  */
-public class MyFirst {
+public class MySecond {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Greetings");
-        System.out.print("Winston is fire 🔥🔥🔥🔥🔥");
-        System.out.println(" >^~^< ");
+        System.out.println("mysecond.MySecond.main()");
     }
-    
+
 }
